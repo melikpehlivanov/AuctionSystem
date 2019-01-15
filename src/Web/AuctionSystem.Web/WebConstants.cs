3d9b2e11c68ca930d4ed9476cb1a4958f755e9b4
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-01-15 10:58:09 +0200

    Fix CategoriesPath

--- src/Web/AuctionSystem.Web/WebConstants.cs
+++ src/Web/AuctionSystem.Web/WebConstants.cs
@@ -2,7 +2,7 @@
 {
     public class WebConstants
     {
-        public const string CategoriesPath = "Resources/seedfiles/categories.json";
+        public const string CategoriesPath = "Resources/SeedFiles/categories.json";
         public const string AdministratorRole = "Administrator";
     }
 }
