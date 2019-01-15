6c76f2ddfc548af0512fd4f591b37af28b10ec4b
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-01-15 10:57:46 +0200

    Fix Category name and refactor

--- src/Web/AuctionSystem.Web/WebConstants.cs
+++ src/Web/AuctionSystem.Web/WebConstants.cs
@@ -2,7 +2,7 @@
 {
     public class WebConstants
     {
-        public const string ManufacturersPath = @"Resources/seedfiles/categories.json";
+        public const string CategoriesPath = "Resources/seedfiles/categories.json";
         public const string AdministratorRole = "Administrator";
     }
 }
