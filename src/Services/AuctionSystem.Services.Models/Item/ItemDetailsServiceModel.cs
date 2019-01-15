271ae0005bf505c495bf28aa6f8f7905a88b31a5
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-01-15 20:14:00 +0200

    Implement item details

--- src/Services/AuctionSystem.Services.Models/Item/ItemDetailsServiceModel.cs
+++ src/Services/AuctionSystem.Services.Models/Item/ItemDetailsServiceModel.cs
@@ -1,7 +1,19 @@
 namespace AuctionSystem.Services.Models.Item
 {
+    using System;
+
     public class ItemDetailsServiceModel : BaseItemServiceModel
     {
         public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal StartingPrice { get; set; }
+
+        public decimal MinIncrease { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
     }
 }
--- src/Web/AuctionSystem.Web/ViewModels/Item/ItemDetailsViewModel.cs
+++ src/Web/AuctionSystem.Web/ViewModels/Item/ItemDetailsViewModel.cs
@@ -1,11 +1,25 @@
 namespace AuctionSystem.Web.ViewModels.Item
 {
+    using System;
     using Common.AutoMapping.Interfaces;
     using Services.Models.Item;
 
     public class ItemDetailsViewModel : IMapWith<ItemDetailsServiceModel>
     {
         public string Id { get; set; }
+
         public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal StartingPrice { get; set; }
+
+        public decimal MinIncrease { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public TimeSpan RemainingTime => DateTime.UtcNow - this.EndTime;
     }
 }
