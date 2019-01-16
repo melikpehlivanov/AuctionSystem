5cf379a06ca172eeacad61efe1ff3be01e6edd6f
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-01-16 11:10:00 +0200

    Implement CategoriesService with GetAllSubCategoriesAsync method

---
+++ src/Services/AuctionSystem.Services.Models/Category/CategoryListingServiceModel.cs
@@ -0,0 +1,7 @@
+namespace AuctionSystem.Services.Models.Category
+{
+    public class CategoryListingServiceModel : BaseCategoryServiceModel
+    {
+        public string Name { get; set; }
+    }
+}
