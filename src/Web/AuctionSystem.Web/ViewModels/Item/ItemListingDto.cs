895704ec1e88651f8f5b385dc4c7920191995af7
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-01-18 23:21:31 +0200

    Add pictures when listing items

--- src/Web/AuctionSystem.Web/ViewModels/Item/ItemListingDto.cs
+++ src/Web/AuctionSystem.Web/ViewModels/Item/ItemListingDto.cs
@@ -1,6 +1,8 @@
 ﻿namespace AuctionSystem.Web.ViewModels.Item
 {
+    using System.Collections.Generic;
     using Common.AutoMapping.Interfaces;
+    using Picture;
     using Services.Models.Item;
 
     public class ItemListingDto : IMapWith<ItemListingServiceModel>
@@ -12,5 +14,7 @@
         public decimal StartingPrice { get; set; }
 
         public string UserFullName { get; set; }
+
+        public ICollection<PictureDisplayViewModel> Pictures { get; set; }
     }
 }
