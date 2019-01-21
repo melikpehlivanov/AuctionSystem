6dba11357df9c1a5a9cc5c05bf5fd6856fa15aa1
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-01-21 20:05:23 +0200

    Add SignalRHub and initial setup for it

---
+++ src/Web/AuctionSystem.Web/SignalRHubs/BidHub.cs
@@ -0,0 +1,18 @@
+﻿namespace AuctionSystem.Web.SignalRHubs
+{
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.SignalR;
+
+    public class BidHub : Hub
+    {
+        public async Task Setup(string consoleId)
+        {
+            if (consoleId == null)
+            {
+                return;
+            }
+
+            await this.Groups.AddToGroupAsync(this.Context.ConnectionId, consoleId);
+        }
+    }
+}
---
+++ src/Web/AuctionSystem.Web/wwwroot/js/bid.js
@@ -0,0 +1,15 @@
+﻿let currentUserId = $('#currentUserId').val();
+let consoleId = $('#consoleId').val();
+
+let connection =
+    new signalR.HubConnectionBuilder()
+        .withUrl('/bidHub')
+        .build();
+
+connection.start()
+    .then(function () {
+        connection.invoke('Setup', consoleId);
+    })
+    .catch(function (err) {
+        return console.error(err.toString());
+    });
