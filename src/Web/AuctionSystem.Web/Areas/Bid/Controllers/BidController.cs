61cc91200c1b15bb7b49412067e42067fa613b38
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-01-21 20:09:08 +0200

    Add initial Bid area

---
+++ src/Web/AuctionSystem.Web/Areas/Bid/Controllers/BidController.cs
@@ -0,0 +1,22 @@
+﻿namespace AuctionSystem.Web.Areas.Bid.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+
+    [Area("Bid")]
+    public class BidController : Controller
+    {
+        [Route("/bid/{id}")]
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return this.NotFound();
+            }
+
+            
+            throw new NotImplementedException();
+        }
+    }
+}
