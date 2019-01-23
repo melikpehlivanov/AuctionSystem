e8cf83667de842bc749c931c7f109872cc5bd828
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-01-23 13:46:46 +0200

    Add remaining time for given item

--- src/Web/AuctionSystem.Web/wwwroot/js/time-calculator.js
+++ src/Web/AuctionSystem.Web/wwwroot/js/time-calculator.js
@@ -1,15 +1,14 @@
-﻿let endTime = moment($('#end-time-id-for-calculator').val());
-let startTime = moment($('#start-time-id-for-calculator').val());
+﻿let endTime = $('#end-time-id-for-calculator').val();
+let startTime = moment.utc($('#start-time-id-for-calculator').val());
 let currentTime = moment();
 
-let eventTime;
 let duration;
-if (endTime > currentTime && startTime < currentTime) {
-    // Calculate remaining time until auction end
-    eventTime = moment.utc(endTime).local();
+if (startTime > currentTime) {
+    // Calculate remaining time until auction start
+    let eventTime = moment.utc(startTime).local();
     duration = moment.duration(eventTime.diff(currentTime));
 } else {
-    eventTime = moment.utc(startTime).local();
+    let eventTime = moment.utc(moment(endTime)).local();
     duration = moment.duration(eventTime.diff(currentTime));
 }
 let interval = 1000;
@@ -23,6 +22,7 @@ setInterval(function () {
     if (duration < 0) {
         location.reload();
     }
+
     $('#countdown')
         .text(`${duration.asDays().toFixed()}d ${duration.hours()}h ${duration.minutes()}m ${duration.seconds()}s`);
 },
