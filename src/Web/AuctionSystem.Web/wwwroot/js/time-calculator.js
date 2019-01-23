bd522996246e7f52618d75b25dc8df0cf0985ccc
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-01-23 12:38:52 +0200

    Small improvements on details page

---
+++ src/Web/AuctionSystem.Web/wwwroot/js/time-calculator.js
@@ -0,0 +1,29 @@
+﻿let endTime = moment($('#end-time-id-for-calculator').val());
+let startTime = moment($('#start-time-id-for-calculator').val());
+let currentTime = moment();
+
+let eventTime;
+let duration;
+if (endTime > currentTime && startTime < currentTime) {
+    // Calculate remaining time until auction end
+    eventTime = moment.utc(endTime).local();
+    duration = moment.duration(eventTime.diff(currentTime));
+} else {
+    eventTime = moment.utc(startTime).local();
+    duration = moment.duration(eventTime.diff(currentTime));
+}
+let interval = 1000;
+let identifier = $('#identifier').val();
+
+setInterval(function () {
+    if (identifier === null) {
+        return;
+    }
+    duration = moment.duration(duration - interval, 'milliseconds');
+    if (duration < 0) {
+        location.reload();
+    }
+    $('#countdown')
+        .text(`${duration.asDays().toFixed()}d ${duration.hours()}h ${duration.minutes()}m ${duration.seconds()}s`);
+},
+    interval);
