a6274e5f36abf58e17a4df446dd84eabbb7a3cc8
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-01-27 15:32:50 +0200

    Rename subCategories variable to categories in categoriesService

--- src/Services/AuctionSystem.Services/Implementations/CategoriesService.cs
+++ src/Services/AuctionSystem.Services/Implementations/CategoriesService.cs
@@ -18,12 +18,12 @@ namespace AuctionSystem.Services.Implementations
         public async Task<IEnumerable<T>> GetAllCategoriesWithSubCategoriesAsync<T>()
             where T : BaseCategoryServiceModel
         {
-            var subCategories = await this.Context.Categories
+            var categories = await this.Context.Categories
                 .OrderBy(c=> c.Name)
                 .ProjectTo<T>()
                 .ToArrayAsync();
 
-            return subCategories;
+            return categories;
         }
     }
 }
