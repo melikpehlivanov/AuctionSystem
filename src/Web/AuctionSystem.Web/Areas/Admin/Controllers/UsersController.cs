88d49bd2b7ce92fbce72793abe1cc965b258814c
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-08 19:07:55 +0200

    Implement RemoveFromRole method in UsersController

--- src/Web/AuctionSystem.Web/Areas/Admin/Controllers/UsersController.cs
+++ src/Web/AuctionSystem.Web/Areas/Admin/Controllers/UsersController.cs
@@ -86,5 +86,45 @@
 
             return this.RedirectToAction(nameof(this.Index));
         }
+        
+        [HttpPost]
+        public async Task<IActionResult> RemoveFromRole(string userEmail, string role)
+        {
+            if (string.IsNullOrWhiteSpace(role) || string.IsNullOrWhiteSpace(userEmail))
+            {
+                this.ShowErrorMessage(NotificationMessages.TryAgainLaterError);
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            var user = await this.userManager.FindByEmailAsync(userEmail);
+            if (user == null)
+            {
+                this.ShowErrorMessage(NotificationMessages.UserNotFound);
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            var currentLoggedUser = await this.userManager.GetUserAsync(this.User);
+
+            if (user.Email == currentLoggedUser.Email)
+            {
+                this.ShowErrorMessage(string.Format(NotificationMessages.UnableToRemoveSelf, role));
+                return this.RedirectToAction(nameof(this.Index));
+            }
+
+            var identityResult = await this.userManager.RemoveFromRoleAsync(user, role);
+
+            var success = identityResult.Succeeded;
+            if (success)
+            {
+                this.ShowSuccessMessage(
+                    string.Format(NotificationMessages.UserRemovedFromRole, userEmail, role));
+            }
+            else
+            {
+                this.ShowErrorMessage(NotificationMessages.TryAgainLaterError);
+            }
+
+            return this.RedirectToAction(nameof(this.Index));
+        }
     }
 }
