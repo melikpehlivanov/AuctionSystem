394d909989e000dd15887f2ff7ece133c55a7200
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-08 17:38:48 +0200

    Add user listing service model and getAllUsersAsync method in IUserService

---
+++ src/Services/AuctionSystem.Services.Models/AuctionUser/UserListingServiceModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AuctionSystem.Services.Models.AuctionUser
+{
+    public class UserListingServiceModel
+    {
+        public string Id { get; set; }
+
+        public string Email { get; set; }
+    }
+}
--- src/Services/AuctionSystem.Services/Interfaces/IUserService.cs
+++ src/Services/AuctionSystem.Services/Interfaces/IUserService.cs
@@ -1,9 +1,13 @@
 ﻿namespace AuctionSystem.Services.Interfaces
 {
+    using System.Collections.Generic;
     using System.Threading.Tasks;
+    using Models.AuctionUser;
 
     public interface IUserService
     {
         Task<string> GetUserIdByUsernameAsync(string username);
+
+        Task<IEnumerable<UserListingServiceModel>> GetAllUsersAsync();
     }
 }
