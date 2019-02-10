98d8a63a1f8c79ad2efdbd3a028fe04a77533b98
Martin Mladenov <30376060+martinmladenov@users.noreply.github.com>
2019-02-10 16:21:27 +0200

    Remove unused files

--- src/Web/AuctionSystem.Web/wwwroot/js/initialize-slick-slider.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    $('.slick').slick({
-        dots: true,
-        slidesToShow: 4,
-        slidesToScroll: 3,
-        autoplay: true,
-        autoplaySpeed: 4000,
-    });
-});
