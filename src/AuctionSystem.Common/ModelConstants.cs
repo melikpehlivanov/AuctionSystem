98d00b644144026991b7e4fbb247cc6d5ce929b5
Melik Pehlivanov <melikpehlivanov@gmail.com>
2019-02-10 21:26:47 +0200

    Make ModelConstants.cs static

--- src/AuctionSystem.Common/ModelConstants.cs
+++ src/AuctionSystem.Common/ModelConstants.cs
@@ -1,24 +1,24 @@
 ﻿namespace AuctionSystem.Common
 {
-    public class ModelConstants
+    public static class ModelConstants
     {
-        public class User
+        public static class User
         {
             public const int FullNameMaxLength = 50;
         }
 
-        public class Bid
+        public static class Bid
         {
             public const string MinAmount = "0.01";
             public const string MaxAmount = "999999999999";
         }
 
-        public class Category
+        public static class Category
         {
             public const int NameMaxLength = 50;
         }
 
-        public class Item
+        public static class Item
         {
             public const int TitleMaxLength = 120;
             public const int DescriptionMaxLength = 500;
@@ -28,7 +28,7 @@
             public const string MaxMinIncrease = "79228162514264337593543950335";
         }
 
-        public class SubCategory
+        public static class SubCategory
         {
             public const int NameMaxLength = 50;
         }
