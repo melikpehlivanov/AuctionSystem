a6b4fc663b47198bffea8f9c7ea412077115b843
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-04-10 21:47:17 +0300

    Create ConfigureCookies extension method

--- src/Web/AuctionSystem.Web/Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Web/AuctionSystem.Web/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -5,7 +5,10 @@
     using Common.EmailSender;
     using Common.EmailSender.Interface;
     using Data;
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Models;
@@ -68,6 +71,21 @@
                     options.ApiSecret = configuration.GetSection("Cloudinary:ApiSecret").Value;
                 })
                 .Configure<SendGridOptions>(options => { options.SendGridApiKey = configuration.GetSection("SendGrid:ApiKey").Value; });
+
+            return services;
+        }
+
+        public static IServiceCollection ConfigureCookies(
+            this IServiceCollection services)
+        {
+            services
+                .Configure<CookiePolicyOptions>(options =>
+                {
+                    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                    options.CheckConsentNeeded = context => true;
+                    options.MinimumSameSitePolicy = SameSiteMode.None;
+                })
+                .Configure<CookieTempDataProviderOptions>(options => { options.Cookie.IsEssential = true; });
 
             return services;
         }
--- src/Web/AuctionSystem.Web/Startup.cs
+++ src/Web/AuctionSystem.Web/Startup.cs
@@ -34,15 +34,7 @@
         {
             services
                 .AddAppSettings(this.Configuration)
-                .Configure<CookiePolicyOptions>(options =>
-                {
-                    // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                    options.CheckConsentNeeded = context => true;
-                    options.MinimumSameSitePolicy = SameSiteMode.None;
-                });
-
-            services
-                .Configure<CookieTempDataProviderOptions>(options => { options.Cookie.IsEssential = true; });
+                .ConfigureCookies();
 
             services
                 .AddDbContext<AuctionSystemDbContext>(options => options
