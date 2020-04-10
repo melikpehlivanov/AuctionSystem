430828283c9861722b62999f45530cb73e127afa
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-04-10 22:04:17 +0300

    Chain services in startup

--- src/Web/AuctionSystem.Web/Startup.cs
+++ src/Web/AuctionSystem.Web/Startup.cs
@@ -1,23 +1,18 @@
 ﻿namespace AuctionSystem.Web
 {
-    using System;
     using AutoMapper;
     using Common.AutoMapping.Profiles;
-    using Common.EmailSender;
     using Data;
     using Infrastructure.Extensions;
     using Infrastructure.Middleware;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
-    using Microsoft.AspNetCore.Http;
-    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Routing;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
-    using Services.Models;
     using SignalRHubs;
 
     public class Startup
@@ -29,48 +24,29 @@
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services
-                .AddAppSettings(this.Configuration)
-                .ConfigureCookies()
-                .ConfigureSecurityStampValidator();
-
-            services
                 .AddDbContext<AuctionSystemDbContext>(options => options
                     .UseSqlServer(this.Configuration.GetDefaultConnectionString()))
-                .AddIdentity();
-
-            services.AddControllersWithViews(options =>
-            {
-                options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>();
-            });
-
-            services.AddRazorPages();
-
-            services
-                .AddDistributedMemoryCache();
-
-            services
+                .AddIdentity()
+                .AddAppSettings(this.Configuration)
+                .ConfigureCookies()
+                .ConfigureSecurityStampValidator()
+                .Configure<RouteOptions>(options => options.LowercaseUrls = true)
+                .AddResponseCompression(options => options.EnableForHttps = true)
+                .AddAutoMapper(typeof(DefaultProfile))
+                .AddDistributedMemoryCache()
                 .AddDomainServices()
                 .AddApplicationServices()
                 .AddCommonProjectServices()
                 .AddAuthentication();
 
-            services
-                .Configure<RouteOptions>(options => options.LowercaseUrls = true);
-
-            services
-                .AddSignalR();
-
-            services
-                .AddResponseCompression(options => options.EnableForHttps = true);
-
-            services.AddAutoMapper(typeof(DefaultProfile));
+            services.AddControllersWithViews(options => { options.Filters.Add<AutoValidateAntiforgeryTokenAttribute>(); });
+            services.AddRazorPages();
+            services.AddSignalR();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -80,40 +56,36 @@
             else
             {
                 app.UseExceptionHandler("/error/500");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
-            app.UseResponseCompression();
-            app.UseStatusCodePagesWithReExecute("/error/{0}");
-            app.AddDefaultSecurityHeaders(
-                new SecurityHeadersBuilder()
-                    .AddDefaultSecurePolicy());
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-            app.UseCookiePolicy();
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapHub<BidHub>("/bidHub");
-                endpoints.MapControllerRoute(
-                    name: "areas",
-                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-                endpoints.MapControllerRoute(
-                    name: "items",
-                    pattern: "Items/{action}/{id}/{slug:required}",
-                    defaults: new { controller = "Items", action = "Details" });
-                endpoints.MapRazorPages();
-            });
-
-            app.SeedData();
+            app.UseResponseCompression()
+                .UseStatusCodePagesWithReExecute("/error/{0}")
+                .AddDefaultSecurityHeaders(
+                    new SecurityHeadersBuilder()
+                        .AddDefaultSecurePolicy())
+                .UseHttpsRedirection()
+                .UseStaticFiles()
+                .UseCookiePolicy()
+                .UseRouting()
+                .UseAuthentication()
+                .UseAuthorization()
+                .UseEndpoints(endpoints =>
+                {
+                    endpoints.MapHub<BidHub>("/bidHub");
+                    endpoints.MapControllerRoute(
+                        "areas",
+                        "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                    endpoints.MapControllerRoute(
+                        "default",
+                        "{controller=Home}/{action=Index}/{id?}");
+                    endpoints.MapControllerRoute(
+                        "items",
+                        "Items/{action}/{id}/{slug:required}",
+                        new { controller = "Items", action = "Details" });
+                    endpoints.MapRazorPages();
+                })
+                .SeedData();
         }
     }
 }
