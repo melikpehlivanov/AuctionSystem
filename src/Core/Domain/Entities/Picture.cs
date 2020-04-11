930fe177eccb0ba7b10e7858b49b7911ed0d86e1
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-04-11 20:13:44 +0300

    Add fluent validation for Picture entity

--- src/Core/Domain/Entities/Picture.cs
+++ src/Core/Domain/Entities/Picture.cs
@@ -1,20 +1,13 @@
 ﻿namespace Domain.Entities
 {
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
     using Common;
 
     public class Picture : AuditableEntity
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
-
-        [Required]
         public string Url { get; set; }
 
-        [Required]
         public string ItemId { get; set; }
-
         public Item Item { get; set; }
     }
 }
---
+++ src/Infrastructure/Persistance/Configurations/PictureConfiguration.cs
@@ -0,0 +1,25 @@
+﻿namespace Persistance.Configurations
+{
+    using Domain.Entities;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class PictureConfiguration : IEntityTypeConfiguration<Picture>
+    {
+        public void Configure(EntityTypeBuilder<Picture> builder)
+        {
+            builder
+                .Property(p => p.Id)
+                .HasColumnName("Id")
+                .ValueGeneratedOnAdd();
+
+            builder
+                .Property(p => p.Url)
+                .IsRequired();
+
+            builder
+                .Property(p => p.ItemId)
+                .IsRequired();
+        }
+    }
+}
