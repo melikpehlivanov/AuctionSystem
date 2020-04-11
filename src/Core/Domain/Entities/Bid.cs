363d34a873747e1e48dd9a088df5dc2b5863eb8d
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-04-11 20:00:02 +0300

    Add fluent validation for Bid entity

--- src/Core/Domain/Entities/Bid.cs
+++ src/Core/Domain/Entities/Bid.cs
@@ -1,29 +1,16 @@
 ﻿namespace Domain.Entities
 {
     using System;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
     using Common;
-    using global::Common;
 
     public class Bid : AuditableEntity
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
-
-        [Required]
-        [Range(typeof(decimal), ModelConstants.Bid.MinAmount, ModelConstants.Bid.MaxAmount)]
         public decimal Amount { get; set; }
-
-        [Required]
         public string UserId { get; set; }
-
-        [Required]
-        public string ItemId { get; set; }
-
-        public Item Item { get; set; }
-
-        [Required]
         public DateTime MadeOn { get; set; }
+
+        public string ItemId { get; set; }
+        public Item Item { get; set; }
     }
 }
---
+++ src/Infrastructure/Persistance/Configurations/BidConfiguration.cs
@@ -0,0 +1,33 @@
+﻿namespace Persistance.Configurations
+{
+    using Domain.Entities;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class BidConfiguration : IEntityTypeConfiguration<Bid>
+    {
+        public void Configure(EntityTypeBuilder<Bid> builder)
+        {
+            builder
+                .Property(p => p.Id)
+                .HasColumnName("Id");
+
+            builder
+                .Property(p => p.Amount)
+                .IsRequired();
+
+            builder
+                .Property(p => p.UserId)
+                .IsRequired();
+
+            builder
+                .Property(p => p.Item)
+                .IsRequired();
+
+            builder
+                .Property(p => p.MadeOn)
+                .IsRequired();
+
+        }
+    }
+}
