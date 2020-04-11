b67a3792434a9f853ca1424e4fb20ca9e32e8540
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-04-11 20:04:21 +0300

    Add fluent validation for Category entity

--- src/Core/Domain/Entities/Category.cs
+++ src/Core/Domain/Entities/Category.cs
@@ -1,20 +1,13 @@
 ﻿namespace Domain.Entities
 {
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
     using Common;
-    using global::Common;
 
     public class Category : AuditableEntity
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
-
-        [Required]
-        [MaxLength(ModelConstants.Category.NameMaxLength)]
         public string Name { get; set; }
 
-        public ICollection<SubCategory> SubCategories { get; set; }
+        public ICollection<SubCategory> SubCategories { get; set; } = new HashSet<SubCategory>();
     }
 }
---
+++ src/Infrastructure/Persistance/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,27 @@
+﻿namespace Persistance.Configurations
+{
+    using Common;
+    using Domain.Entities;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder
+                .Property(p => p.Id)
+                .HasColumnName("Id")
+                .ValueGeneratedOnAdd();
+
+            builder
+                .Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(ModelConstants.Category.NameMaxLength);
+
+            builder
+                .HasMany(x => x.SubCategories)
+                .WithOne(c => c.Category);
+        }
+    }
+}
