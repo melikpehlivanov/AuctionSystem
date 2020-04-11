fc4e8e5713ad9a436c4e5dc7ac394cc4005b4154
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-04-11 18:29:06 +0300

    Implement dbcontext factory

---
+++ src/Infrastructure/Persistance/AuctionSystemDbContextFactory.cs
@@ -0,0 +1,12 @@
+﻿namespace Persistance
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class AuctionSystemDbContextFactory : DesignTimeDbContextFactoryBase<AuctionSystemDbContext>
+    {
+        protected override AuctionSystemDbContext CreateNewInstance(DbContextOptions<AuctionSystemDbContext> options)
+        {
+            return new AuctionSystemDbContext(options);
+        }
+    }
+}
---
+++ src/Infrastructure/Persistance/DesignTimeDbContextFactory.cs
@@ -0,0 +1,55 @@
+﻿namespace Persistance
+{
+    using System;
+    using System.IO;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Design;
+    using Microsoft.Extensions.Configuration;
+
+    public abstract class DesignTimeDbContextFactoryBase<TContext> :
+        IDesignTimeDbContextFactory<TContext> where TContext : DbContext
+    {
+        private const string ConnectionStringName = "DefaultConnection";
+        private const string AspNetCoreEnvironment = "ASPNETCORE_ENVIRONMENT";
+
+        public TContext CreateDbContext(string[] args)
+        {
+            var basePath = Directory.GetCurrentDirectory() + string.Format("{0}..{0}AuctionSystem.Web", Path.DirectorySeparatorChar);
+            return this.Create(basePath, Environment.GetEnvironmentVariable(AspNetCoreEnvironment));
+        }
+
+        protected abstract TContext CreateNewInstance(DbContextOptions<TContext> options);
+
+        private TContext Create(string basePath, string environmentName)
+        {
+
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(basePath)
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.Local.json", optional: true)
+                .AddJsonFile($"appsettings.{environmentName}.json", optional: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var connectionString = configuration.GetConnectionString(ConnectionStringName);
+
+            return this.Create(connectionString);
+        }
+
+        private TContext Create(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new ArgumentException($"Connection string '{ConnectionStringName}' is null or empty.", nameof(connectionString));
+            }
+
+            Console.WriteLine($"DesignTimeDbContextFactoryBase.Create(string): Connection string: '{connectionString}'.");
+
+            var optionsBuilder = new DbContextOptionsBuilder<TContext>();
+
+            optionsBuilder.UseSqlServer(connectionString);
+
+            return this.CreateNewInstance(optionsBuilder.Options);
+        }
+    }
+}
