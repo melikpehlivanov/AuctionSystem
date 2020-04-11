d27105d227c1ab75961e7420598b9db7d165f071
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-04-11 18:21:31 +0300

    Implement AddPersistence extension method

---
+++ src/Infrastructure/Persistance/DependencyInjection.cs
@@ -0,0 +1,20 @@
+﻿namespace Persistance
+{
+    using Application.Common.Interfaces;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AuctionSystemDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddScoped<IAuctionSystemDbContext>(provider => provider.GetService<AuctionSystemDbContext>());
+
+            return services;
+        }
+    }
+}
