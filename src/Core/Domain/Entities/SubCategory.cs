ac5f221b6c3a631ff0d01b47ada9174853b06947
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-04-11 20:16:19 +0300

    Add fluent validation for SubCategory entity

--- src/Core/Domain/Entities/SubCategory.cs
+++ src/Core/Domain/Entities/SubCategory.cs
@@ -1,24 +1,16 @@
 ﻿namespace Domain.Entities
 {
     using System.Collections.Generic;
-    using System.ComponentModel.DataAnnotations;
-    using System.ComponentModel.DataAnnotations.Schema;
     using Common;
-    using global::Common;
 
     public class SubCategory : AuditableEntity
     {
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
-
-        [Required]
-        [MaxLength(ModelConstants.SubCategory.NameMaxLength)]
         public string Name { get; set; }
 
         public string CategoryId { get; set; }
-
         public Category Category { get; set; }
-        
-        public ICollection<Item> Items { get; set; }
+
+        public ICollection<Item> Items { get; set; } = new HashSet<Item>();
     }
 }
---
+++ src/Infrastructure/Persistance/Configurations/SubCategoryConfiguration.cs
@@ -0,0 +1,27 @@
+﻿namespace Persistance.Configurations
+{
+    using Common;
+    using Domain.Entities;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+    public class SubCategoryConfiguration : IEntityTypeConfiguration<SubCategory>
+    {
+        public void Configure(EntityTypeBuilder<SubCategory> builder)
+        {
+            builder
+                .Property(p => p.Id)
+                .HasColumnName("Id")
+                .ValueGeneratedOnAdd();
+
+            builder
+                .Property(p => p.Name)
+                .IsRequired()
+                .HasMaxLength(ModelConstants.SubCategory.NameMaxLength);
+
+            builder
+                .Property(p => p.CategoryId)
+                .IsRequired();
+        }
+    }
+}
