4a8055456f2535a2665f4e82827b59af2fcb93d3
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-04-12 11:06:45 +0300

    Add overload in NotFoundException

--- src/Core/Application/Common/Exceptions/NotFoundException.cs
+++ src/Core/Application/Common/Exceptions/NotFoundException.cs
@@ -4,6 +4,11 @@
 
     public class NotFoundException : Exception
     {
+        public NotFoundException(string name)
+            : base($"Entity \"{name}\" was not found.")
+        {
+        }
+
         public NotFoundException(string name, object key)
             : base($"Entity \"{name}\" ({key}) was not found.")
         {
