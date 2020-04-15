26be2805dacddb4caa6dbe74a37e303fd7001d0e
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-04-15 10:43:50 +0300

    Create UpdateItemCommand and UpdateItemCommandValidator

---
+++ src/Core/Application/Items/Commands/UpdateItem/UpdateItemCommand.cs
@@ -0,0 +1,25 @@
+﻿namespace Application.Items.Commands.UpdateItem
+{
+    using System;
+    using Common.Models;
+    using MediatR;
+
+    public class UpdateItemCommand : IRequest<Response<ItemResponseModel>>
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal StartingPrice { get; set; }
+
+        public decimal MinIncrease { get; set; }
+
+        public DateTime StartTime { get; set; }
+
+        public DateTime EndTime { get; set; }
+
+        public Guid SubCategoryId { get; set; }
+    }
+}
---
+++ src/Core/Application/Items/Commands/UpdateItem/UpdateItemCommandValidator.cs
@@ -0,0 +1,34 @@
+﻿namespace Application.Items.Commands.UpdateItem
+{
+    using FluentValidation;
+    using global::Common;
+
+    public class UpdateItemCommandValidator : AbstractValidator<UpdateItemCommand>
+    {
+        private readonly IDateTime dateTime;
+
+        public UpdateItemCommandValidator(IDateTime dateTime)
+        {
+            this.dateTime = dateTime;
+
+            this.RuleFor(p => p.Id).NotNull();
+            this.RuleFor(p => p.Title).NotNull().MaximumLength(ModelConstants.Item.TitleMaxLength);
+            this.RuleFor(p => p.Description).NotNull().MaximumLength(ModelConstants.Item.DescriptionMaxLength);
+            this.RuleFor(p => p.StartingPrice).NotNull()
+                .InclusiveBetween(ModelConstants.Item.MinStartingPrice, ModelConstants.Item.MaxStartingPrice);
+            this.RuleFor(p => p.MinIncrease).NotNull()
+                .InclusiveBetween(ModelConstants.Item.MinMinIncrease, ModelConstants.Item.MaxMinIncrease);
+
+            this.RuleFor(p => p.StartTime).NotNull();
+            this.RuleFor(p => p.EndTime).NotNull();
+
+            this.RuleFor(m => new { m.StartTime, m.EndTime }).NotNull()
+                .Must(x => x.EndTime.Date.ToUniversalTime() >= x.StartTime.Date.ToUniversalTime())
+                .WithMessage("End time must be after start time")
+                .Must(x => x.StartTime.ToUniversalTime() >= this.dateTime.Now.ToUniversalTime())
+                .WithMessage("The Start time must be after the current time");
+
+            this.RuleFor(p => p.SubCategoryId).NotNull();
+        }
+    }
+}
