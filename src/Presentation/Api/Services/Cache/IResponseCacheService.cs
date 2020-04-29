fc6dc3fc8b49b3b1e921e80e6117adb19644ad78
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-04-29 14:40:13 +0300

    Implement ResponseCacheService

--- src/Presentation/Api/Services/Cache/IResponseCacheService.cs
+++ src/Presentation/Api/Services/Cache/IResponseCacheService.cs
@@ -1,6 +1,12 @@
 ﻿namespace Api.Services.Cache
 {
+    using System;
+    using System.Threading.Tasks;
+
     public interface IResponseCacheService
     {
+        Task CacheResponseAsync(string key, object response, TimeSpan cachingTime);
+
+        Task<string> GetCachedResponseAsync(string cacheKey);
     }
 }
--- src/Presentation/Api/Services/Cache/ResponseCacheService.cs
+++ src/Presentation/Api/Services/Cache/ResponseCacheService.cs
@@ -1,6 +1,40 @@
 ﻿namespace Api.Services.Cache
 {
+    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Caching.Distributed;
+    using Newtonsoft.Json;
+
     public class ResponseCacheService : IResponseCacheService
     {
+        private readonly IDistributedCache distributedCache;
+
+        public ResponseCacheService(IDistributedCache distributedCache)
+        {
+            this.distributedCache = distributedCache;
+        }
+
+        public async Task CacheResponseAsync(string key, object response, TimeSpan cachingTime)
+        {
+            if (response == null)
+            {
+                return;
+            }
+
+            await this.distributedCache
+                .SetStringAsync(
+                    key, 
+                    JsonConvert.SerializeObject(response),
+                    new DistributedCacheEntryOptions
+                    {
+                        AbsoluteExpirationRelativeToNow = cachingTime
+                    });
+        }
+
+        public async Task<string> GetCachedResponseAsync(string cacheKey)
+        {
+            var cachedResponse = await this.distributedCache.GetStringAsync(cacheKey);
+            return string.IsNullOrEmpty(cachedResponse) ? null : cachedResponse;
+        }
     }
 }
