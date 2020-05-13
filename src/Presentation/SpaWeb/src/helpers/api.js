1cd42801b3b6d2c399f2b28ec10803fc8d88e532
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-05-13 15:49:56 +0300

    Add error handling in axios interceptor

--- src/Presentation/SpaWeb/src/helpers/api.js
+++ src/Presentation/SpaWeb/src/helpers/api.js
@@ -1,5 +1,22 @@
 import axios from "axios";
+import { toast } from "react-toastify";
 
-export default axios.create({
+const Axios = axios.create({
   baseURL: "https://localhost:5001/api",
+  headers: {
+    "Content-type": "application/json",
+  },
 });
+
+Axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const errorResponse = error.response;
+    if (errorResponse.status) {
+      toast.error(errorResponse.data.errors);
+      return Promise.reject(error);
+    }
+  }
+);
+
+export default Axios;
