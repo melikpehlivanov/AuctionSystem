ab17b07b0448dbe2658332adcd7ac431b90ea28c
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-05-15 13:23:42 +0300

    Convert start and end time in CreateItemCommand to universal time

--- src/Core/Application/Items/Commands/CreateItem/CreateItemCommand.cs
+++ src/Core/Application/Items/Commands/CreateItem/CreateItemCommand.cs
@@ -2,13 +2,14 @@
 {
     using System;
     using System.Collections.Generic;
+    using AutoMapper;
     using Common.Models;
     using Domain.Entities;
     using global::Common.AutoMapping.Interfaces;
     using MediatR;
     using Microsoft.AspNetCore.Http;
 
-    public class CreateItemCommand : IRequest<Response<ItemResponseModel>>, IMapWith<Item>
+    public class CreateItemCommand : IRequest<Response<ItemResponseModel>>, IMapWith<Item>, IHaveCustomMapping
     {
         public string Title { get; set; }
 
@@ -25,5 +26,15 @@
         public Guid SubCategoryId { get; set; }
 
         public ICollection<IFormFile> PictureFormFiles { get; set; } = new HashSet<IFormFile>();
+
+        public void ConfigureMapping(Profile mapper)
+        {
+            mapper
+                .CreateMap<CreateItemCommand, Item>()
+                .ForMember(dest => dest.StartTime,
+                    opt => opt.MapFrom(src => src.StartTime.ToUniversalTime()))
+                .ForMember(dest => dest.EndTime,
+                    opt => opt.MapFrom(src => src.EndTime.ToUniversalTime()));;
+        }
     }
 }
