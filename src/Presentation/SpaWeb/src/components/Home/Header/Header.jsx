8925d05036633f484d5a65a0c1644f1961d63182
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-06-17 20:11:27 +0300

    Add loading state in Header.jsx

--- src/Presentation/SpaWeb/src/components/Home/Header/Header.jsx
+++ src/Presentation/SpaWeb/src/components/Home/Header/Header.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Nav, Navbar, NavDropdown } from "react-bootstrap";
+import { Nav, Navbar, NavDropdown, Spinner } from "react-bootstrap";
 import categoriesService from "../../../services/categoriesService";
 import { history } from "../../..";
 import "./Header.css";
@@ -7,6 +7,7 @@ import "./Header.css";
 export const Header = () => {
   const [categories, setCategories] = useState([]);
   const [hoveredDropdowns, setHoveredDropdowns] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     retrieveCategories();
@@ -15,6 +16,7 @@ export const Header = () => {
   const retrieveCategories = () => {
     categoriesService.getAll().then((response) => {
       setCategories(response.data.data);
+      setIsLoading(false);
     });
   };
 
@@ -36,32 +38,38 @@ export const Header = () => {
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Nav className="mx-auto">
-          {categories.map((category, index) => {
-            return (
-              <NavDropdown
-                key={index}
-                id={index}
-                title={category.name}
-                show={hoveredDropdowns[index]}
-                onMouseOver={(e) => handleMouseOver(e)}
-                onMouseOut={(e) => handleMouseOut(e)}
-              >
-                {category.subCategories.map((subcategory, index) => {
-                  return (
-                    <NavDropdown.Item
-                      id={subcategory.id}
-                      key={index}
-                      onClick={(e) => {
-                        history.push(`/items/${e.target.id}`);
-                      }}
-                    >
-                      {subcategory.name}
-                    </NavDropdown.Item>
-                  );
-                })}
-              </NavDropdown>
-            );
-          })}
+          {isLoading ? (
+            <div>
+              Categories are being loaded... <Spinner animation="grow" />
+            </div>
+          ) : (
+            categories.map((category, index) => {
+              return (
+                <NavDropdown
+                  key={index}
+                  id={index}
+                  title={category.name}
+                  show={hoveredDropdowns[index]}
+                  onMouseOver={(e) => handleMouseOver(e)}
+                  onMouseOut={(e) => handleMouseOut(e)}
+                >
+                  {category.subCategories.map((subcategory, index) => {
+                    return (
+                      <NavDropdown.Item
+                        id={subcategory.id}
+                        key={index}
+                        onClick={(e) => {
+                          history.push(`/items/${e.target.id}`);
+                        }}
+                      >
+                        {subcategory.name}
+                      </NavDropdown.Item>
+                    );
+                  })}
+                </NavDropdown>
+              );
+            })
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
