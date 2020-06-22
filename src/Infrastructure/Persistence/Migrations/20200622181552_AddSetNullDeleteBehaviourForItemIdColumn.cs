e91a65d68838de773eb851c4048351fd978e690e
Melik Pehlivanov <melikpehlivanov@gmail.com>
2020-06-22 21:25:31 +0300

    Make ItemId property nullable

---
+++ src/Infrastructure/Persistence/Migrations/20200622181552_AddSetNullDeleteBehaviourForItemIdColumn.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Persistence.Migrations
+{
+    public partial class AddSetNullDeleteBehaviourForItemIdColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bids_Items_ItemId",
+                table: "Bids");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ItemId",
+                table: "Bids",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bids_Items_ItemId",
+                table: "Bids",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bids_Items_ItemId",
+                table: "Bids");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "ItemId",
+                table: "Bids",
+                type: "uniqueidentifier",
+                nullable: false,
+                oldClrType: typeof(Guid),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bids_Items_ItemId",
+                table: "Bids",
+                column: "ItemId",
+                principalTable: "Items",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
